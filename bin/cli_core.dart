c8cb0d0a349a462768f8a8032642f8e2dac13371
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2022-11-10 23:32:10 +0330

    [Init]

---
+++ bin/cli_core.dart
@@ -0,0 +1,5 @@
+import 'package:cli_core/cli_core.dart' as cli_core;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cli_core.calculate()}!');
+}
---
+++ test/cli_core_test.dart
@@ -0,0 +1,8 @@
+import 'package:cli_core/cli_core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
